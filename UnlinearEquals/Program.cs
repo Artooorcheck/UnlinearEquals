90b4441264130190ef1fa1be39f6989a7f9b8ba8
Tests000 <83328245+Tests000@users.noreply.github.com>
2022-02-23 12:42:14 +0300

    Добавьте файлы проекта.

---
+++ UnlinearEquals/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnlinearEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите X0");
+            var x = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите максимальную погрешность");
+            var eps = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите максимальное число итераций");
+            var k = Double.Parse(Console.ReadLine());
+
+            var eq = new SolveEquals(x, eps, k);
+
+            if (!eq.OnError)
+                Console.WriteLine($"Решение = {eq.xx}\nЧисло итераций = {eq.l}\nF(x) = {eq.fx}");
+            else
+                Console.WriteLine(eq.IER);
+            Console.Read();
+        }
+    }
+}
---
+++ UnlinearEquals/SolveEquals.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnlinearEquals
+{
+    class SolveEquals
+    {
+        private int ier;
+        public string IER
+        {
+            get
+            {
+                switch (ier)
+                {
+                    case 0:
+                        return "Ошибок нет";
+                    case 1:
+                        return "Превышено число итераций";
+                    case 2:
+                        return "Деление на \"0\"";
+                    case 3:
+                        return "Не удалось вычислить X";
+                    default:
+                        return "Неизвестная ошибка";
+                }
+            }
+        }
+
+        public bool OnError
+        {
+            get
+            {
+                return ier != 0;
+            }
+        }
+
+        public double l;
+        public double xx;
+        public double fx;
+
+        private double F(double x)
+        {
+            return x * x * x - 3 * x + 4;
+            //return Math.Sin(x);
+            //return Math.Exp(x)-5*x;
+        }
+
+        private double DF(double x)
+        {
+            return 3 * x * x - 3;
+            //return Math.Cos(x);
+            //return Math.Exp(x)-5;
+        }
+
+        public SolveEquals(double x0, double E, double k)
+        {
+            xx = x0;
+            var df0 = DF(x0);
+            if (df0 == 0)
+            {
+                ier = 2;
+                return;
+            }
+            for (int i = 1; i <= k; i++)
+            {
+                var lxx = xx;
+                xx = lxx - F(lxx) / df0;
+                if (Double.IsNaN(xx) || Double.IsInfinity(xx))
+                {
+                    ier = 3;
+                    return;
+                }
+
+                var Eps = Math.Abs(xx - lxx);
+                l = i;
+                if (Eps <= E)
+                {
+                    fx = F(xx);
+                    return;
+                }
+            }
+            ier = 1;
+        }
+    }
+}
